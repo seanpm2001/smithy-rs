01112a49f682449c962283552c8ac1dd0c99f020
david-perez <d@vidp.dev>
2023-02-02 17:29:51 +0100

    client-tests work

---
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/customizations/CustomValidationExceptionWithReasonDecoratorTest.kt
@@ -0,0 +1,10 @@
+package software.amazon.smithy.rust.codegen.server.smithy.customizations
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class CustomValidationExceptionWithReasonDecoratorTest {
+    @Test
+    fun `constrained model with the CustomValidationExceptionWithReasonDecorator applied compiles`() {
+    }
+}
